9895893bdf616c28d42ff7b640cffc4e207fff6d
Dave Mateer <davemateer@gmail.com>
2025-11-27 11:57:24 +0000

    test server

--- go.sh
+++ go.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# this is for the test webserver to pull latest code and restart the server
+
 # Kill any existing instances
 echo "Killing anything on port 3000..."
 sudo netstat -tulnp | grep 3000 | awk '{print $7}' | cut -d'/' -f1 | xargs -r sudo kill -9
@@ -23,9 +25,10 @@ uv sync
 
 echo "Step 5: Starting FastAPI server..."
 
-# Set production environment
-export ENVIRONMENT=production
+# Set test environment
+export ENVIRONMENT=test
 
 # see notes on workers for production deployment
+# have hacked in 3000 (not 8000) to as just easier for test as old process for node ran on this.
 uv run uvicorn main:app --host 0.0.0.0 --port 3000
 
--- infra/sshCurrentVm.sh
+++ infra/sshCurrentVm.sh
@@ -1,3 +1,3 @@
 
-ssh -o StrictHostKeyChecking=no dave@evidenceplatformweb177.westeurope.cloudapp.azure.com
+ssh -o StrictHostKeyChecking=no dave@evidenceplatformweb987.westeurope.cloudapp.azure.com
 
